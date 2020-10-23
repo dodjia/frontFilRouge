31aac94f37aa2c5e0d96c116e53fe833336d7f9c
Hugo <Hugo.Fillinger@uha.fr>
2020-10-23 11:43:15 +0200

    Création d'un canvas et ajout de form possible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Component/home/home.component';
+import { PaintComponent } from './Component/paint/paint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PaintComponent
   ],
   imports: [
     BrowserModule,
